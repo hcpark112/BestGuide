9bb9568da0d05dbb2fe32042862ca042ea959d4a
Jessica SH Kim <jesskim2613@gmail.com>
2019-02-02 15:00:11 -0800

    Update postings.js

--- js/postings.js
+++ js/postings.js
@@ -12,24 +12,23 @@ var postingsRef = firebase.database().ref('postings');
 
 var count = 0;
 var row = 0;
-
+var length = 0;
 var url = document.location.href;
+console.log(url);
 var category = url.split('#')[1];
+console.log(category);
 
 postingsRef.child(category).once("value", function(snapshot) {
   snapshot.forEach(function(childSnapshot) {
-    let arr = [];
-    childSnapshot.forEach(function(snap) {
-      arr.push(snap.val());
-    });
-    
-    // console.log(count++);
-    if(count == 4){
+
+
+    if(count % 4 == 0 && count!=0){
       row++;
       count = 0;
-    }
-    else{
-
+      let arr = [];
+      childSnapshot.forEach(function(snap) {
+        arr.push(snap.val());
+      });
       console.log(arr[1] + " " + arr[2]);
       let card = $("<div class='card ml-1 mb-2' style='width: 18rem;'></div>");
       let cardBody = $("<div class='card-body'></div>");
@@ -48,12 +47,41 @@ postingsRef.child(category).once("value", function(snapshot) {
       $(cardBody).append(h5);
       $(cardBody).append(p);
       $(cardBody).append(a);
-      console.log(row);
-      console.log(count);
+      console.log('row '+row);
+      console.log('count '+count);
+      count++;
+
+    }
+    else{
+      let arr = [];
+      childSnapshot.forEach(function(snap) {
+        arr.push(snap.val());
+      });
+      console.log(arr[1] + " " + arr[2]);
+      let card = $("<div class='card ml-1 mb-2' style='width: 18rem;'></div>");
+      let cardBody = $("<div class='card-body'></div>");
+      let h5 = $("<h5 class='card-title'></h5>");
+      let p = $("<p class='card-text'></p>");
+      let a = $("<a class='btn btn-primary'></a>");
+
+      $(h5).html(arr[1] + " " + arr[2]);
+      $(p).html("Phone: " + arr[3] + "</br>Skill: " + arr[4]);
+      $(a).html("Contact");
+
+      $(a).attr("href", "mailto:"+arr[0]);
+
+      $("#tester"+row).append(card);
+      $(card).append(cardBody);
+      $(cardBody).append(h5);
+      $(cardBody).append(p);
+      $(cardBody).append(a);
+      console.log('row '+row);
+      console.log('count '+count);
       count++;
     }
   });
 });
+
 
 // function loopForUserGroups(currentUserRef) {
 //    currentUserRef.child('groups').once('value').then(function(snapshot) {
