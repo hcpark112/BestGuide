387d989cd0f218311a2323aaae6a0bbc9c77fcc7
damon ren <demiao.ren@gmail.com>
2019-02-02 14:32:37 -0800

    minor fixes

--- js/categories.js
+++ js/categories.js
@@ -1,13 +1,18 @@
 
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyBW4SwDx_bHK5q2rGUaIpkkd1f5oQtOgPI",
-  authDomain: "bestguide-5cf48.firebaseapp.com",
-  databaseURL: "https://bestguide-5cf48.firebaseio.com",
-  projectId: "bestguide-5cf48",
-  storageBucket: "bestguide-5cf48.appspot.com",
-  messagingSenderId: "317780907451"
-};
-firebase.initializeApp(config);
+$(function() {
+  //listens for user authentication status.
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+     console.log(user.uid);
+    //loopForUserAgendas(currentUserRef);
+    } else {
+      window.location.href = "./index.html";
+    }
+  });
 
-console.log(firebase.auth().currentUser.uid);
+  $(document).on('click', '.custom-card',
+  function redirect() {
+    window.location.href = "./postings.html#" + $(this).attr('id');
+  });
+});
+  
