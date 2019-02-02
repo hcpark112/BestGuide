25f7ebfc94adbb5c365ee24361e686f65b0fce8b
damon ren <demiao.ren@gmail.com>
2019-02-01 21:03:31 -0800

    sets up firebase

---
+++ js/firebase_setup.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBW4SwDx_bHK5q2rGUaIpkkd1f5oQtOgPI",
+  authDomain: "bestguide-5cf48.firebaseapp.com",
+  databaseURL: "https://bestguide-5cf48.firebaseio.com",
+  projectId: "bestguide-5cf48",
+  storageBucket: "bestguide-5cf48.appspot.com",
+  messagingSenderId: "317780907451"
+};
+firebase.initializeApp(config);
